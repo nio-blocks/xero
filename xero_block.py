065de8fe41cb0e96d161ba213d9c9f494d73a70a
cowleyk <cowley.kevin.l@gmail.com>
2017-11-22 10:31:48 -0700

    Posting to Xero, somehow screwing simulator block

--- xero_block.py
+++ xero_block.py
@@ -2,7 +2,7 @@ import json
 from xero import Xero
 from xero.auth import PrivateCredentials
 
-from nio.block.base import Block
+from nio import Block
 from nio.properties import VersionProperty, ObjectProperty, StringProperty, \
     PropertyHolder, FloatProperty
 
@@ -41,29 +41,64 @@ class XeroPut(Block):
     line_items = ObjectProperty(LineItems, title='Invoice Line Item')
 
     def __init__(self):
-        super().__init__()
         self.xero = None
-        self.json = None
+        self.credentials = None
+        super().__init__()
+
+    def configure(self, context):
+        super().configure(context)
+        print('@@@@@@@@@@@')
+        print('CONFIGURE')
+        print('@@@@@@@@@@@')
+        # json_dict = {}
+        con_key = "BHLXMHJQPUUBM8ZKVBJCQLKTN860JE"
+        rsa_private_key = '-----BEGIN RSA PRIVATE KEY-----\nMIICXgIBAAKBgQC5zDwUVG6XvG8QJJ3TIj66JfmIrHWLeCqiTMleF7R2/SbVVynX\neS5v59yEGccfIArsuG9sHWczizvMSa31JJCPdvyRCXm06n4dTSXABSFZ6rQsjeJI\n3tvEuxNCvvZYRjwk1NWpaYPSslx25CE/Bh8M5Np6IwvtlwcG+6lRj8UFwwIDAQAB\nAoGBAI7EoDni8yRHmHQoHtpZSygQ/CEInD4ydVhHdsiFoJd6STfQBYfcR1GYMfuU\nL0z8e0iRJJVINsAFskp1J2Xi1e0GsunwNaBV6npOFjP/Ta2C1BIqRClW1IR/gmCk\nuX9sBb6Edg+xDriQyIckhbR4eivj0FQprZvl4XbdfP8UDkABAkEA8ksbWWrn9i4M\n2bdv9MVBKltFl4mVBJdhGnCwPE1sscLGLmXgS546hGV1/Ok5UtK8yKoYe8ViMWsS\nuvzFKzjlwwJBAMRO9WURd3eqkI/6LgRiejhGNc+NefOaolj6iVLwuAaKu3LKWjwg\nV19dp7AArFORQ39+ermnUPG/0cq0/orhYAECQQCamXPs/TrfKQkIDnUAULDA7xYb\nmC0ejdzmuwqon5qAXlCIIHcaqO6btgWwB7yM7WyJ+Ya/yvVZpQGBkHHan2ZzAkAg\nD0EWW27mVS28xb/kKW0KabT6C4HiHdvrqibpK7TyJJAOZCuubB24zmPHY6TBFRUv\n6ikCzudyQ8BwWXTEm6ABAkEAkJhnQujZd5qBiEfR1gdNApqQQ8SrVE2gNDUNsb3/\nqpAP6WlWybok798iPoBzMWJdPpXaZe8V1oe/1/cB3S1Z8A==\n-----END RSA PRIVATE KEY-----\n'
+        self.credentials = PrivateCredentials(con_key, rsa_private_key)
+        self.xero = Xero(self.credentials)
+        # json_dict['Type'] = self.invoice_type()
+        # json_dict['Contact'] = {'Name': self.contact_name()}
+        # json_dict['LineItems'] = {
+        #     'Description': self.line_items().description(),
+        #     'UnitAmount': self.line_items().unit_amount(),
+        #     'TaxAmount': self.line_items().tax_amount()
+        # }
+        # json_dict['Status'] = self.status()
+
+        # self.json = json.loads(json_dict)
+        resp_signal = self.xero.invoices.put({
+            'Type': 'ACCREC',
+            'Contact': {
+                'Name': 'Kevin Force'
+            },
+            'LineItems': [{
+                'Description': 'Force Description',
+                'Quantity': 4,
+                'UnitAmount': 9.99,
+                'TaxAmount': 1.11
+            }]
+        })
+        print('%%%%%%%%%%')
+        print(resp_signal)
+        print('%%%%%%%%%%')
 
     def start(self):
-        credentials = PrivateCredentials(self.consumer_key(), self.rsa_key())
-        self.xero = Xero(credentials)
+        super().start()
+        # self.xero = Xero(self.credentials)
+        print('&&&&&&&&&&')
+        print('START')
+        print('&&&&&&&&&&')
 
     def process_signals(self, signals):
+        print('!!!!!!!!!!')
+        print('PROCESS_SIGNALS')
+        print('!!!!!!!!!!')
         json_dict = {}
         for signal in signals:
-            json_dict['Type'] = self.invoice_type(signal)
-            json_dict['Contact'] = {'Name': self.contact_name(signal)}
-            json_dict['LineItems'] = {
-                'Description': self.line_items().description(signal),
-                'UnitAmount': self.line_items().unit_amount(signal),
-                'TaxAmount': self.line_items().tax_amount(signal)
-            }
-            json_dict['Status'] = self.status(signal)
+            pass
+        # self.notify_signals(resp_signal)
 
-            # self.json = json.loads(json_dict)
-            resp_signal = self.xero.invoices.put(json_dict)
-        self.notify_signals(resp_signal)
+
+# TODO: Turn this into a base block?
 
 # "Private Application"
     # Generate RSA bs and create key in app.xero.com
